661ea9e1850151784ea0ac4ed9519731cacb65e1
SMakaiTakori <makait87@icloud.com>
2021-05-02 17:53:50 -0400

    added columns component

---
+++ src/components/columns.js
@@ -0,0 +1,34 @@
+export const COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+  },
+  {
+    Header: "OPO",
+    accessor: "OPO",
+  },
+  {
+    Header: "Tier",
+    accessor: "Tier",
+  },
+  {
+    Header: "State(s)",
+    accessor: "State_s_",
+  },
+  {
+    Header: "Patients on waitlist",
+    accessor: "Patients_on_waitlist",
+  },
+  {
+    Header: "Donors needed",
+    accessor: "Donors_needed",
+  },
+  {
+    Header: "Organs needed",
+    accessor: "Organs_needed",
+  },
+  {
+    Header: "Notes",
+    accessor: "Notes",
+  },
+]
