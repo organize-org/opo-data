10880cc2b0f891094da495a8e6caa2a6282df246
SMakaiTakori <makait87@icloud.com>
2021-06-09 17:49:35 -0400

    code cleanup, moved transform, getColor and tierData to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,50 @@ import Table from "../components/table";
 import Map from "../components/map";
 
 export default function Dashboard({ data }) {
+  const geoData = data.allGeoJson.edges.map(({ node }) => ({ ...node }));
+  const tableData = data.allMetricsCsv.edges.map(({ node }) => ({ ...node }));
+
+  const tierData = tableData.reduce(
+    (tableDataMap, tableRow) => ({
+      ...tableDataMap,
+      [tableRow.OPO]: tableRow,
+    }),
+    {}
+  );
+
+  const transformed = {
+    ...geoData[0],
+    features: geoData[0].features.map(feature => ({
+      ...feature,
+      properties: {
+        ...feature.properties,
+        tier: tierData[feature.properties.name]?.Tier,
+      },
+    })),
+  };
+
+  const getColor = tier => {
+    switch (tier) {
+      case "3 Failing":
+        return "#D43C37";
+      case "2 Underperforming":
+        return "#FFB042";
+      case "1 Passing":
+        return "#C4C4C4";
+      default:
+        return "green";
+    }
+  };
+
   return (
     <Layout>
       <Map
-        geoData={data.allGeoJson.edges.map(({ node }) => ({ ...node }))}
-        tableData={data.allMetricsCsv.edges.map(({ node }) => ({ ...node }))}
+        geoData={geoData}
+        tableData={tableData}
+        transformed={transformed}
+        getColor={getColor}
       />
-      <Table data={data.allMetricsCsv.edges.map(({ node }) => ({ ...node }))} />
+      <Table data={tableData} />
     </Layout>
   );
 }
