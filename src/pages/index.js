4b1e7648f795a4b12a9bc60c106184ca2fbd46a4
SMakaiTakori <makait87@icloud.com>
2021-06-15 16:27:14 -0400

    added global css file, deleted table file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 
 import Layout from "../components/layout";
-import Table from "../components/table";
+
 import Map from "../components/map";
 
 export default function Dashboard({ data }) {
@@ -31,7 +31,6 @@ export default function Dashboard({ data }) {
   return (
     <Layout>
       <Map geoData={transformedGeoData} />
-      <Table data={tableData} />
     </Layout>
   );
 }
