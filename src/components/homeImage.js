c20ae4086432c6cedfa40ed6cfa3bce7298b7942
SMakaiTakori <makait87@icloud.com>
2021-06-21 13:21:59 -0400

    added StaticImage

--- src/components/homeImage.js
+++ src/components/homeImage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 
+import * as homeStyles from "../components/home/home.module.css";
+
 export default function HomeImage() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className={homeStyles.imgBackground}>
       <StaticImage src="../images/home-img.png" alt="sick man" />
     </div>
   );
