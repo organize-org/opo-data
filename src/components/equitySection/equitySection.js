2b7a91bb4118b4865d589a1e39de5e245e71fd94
vonbearshark <jjnaughton93@gmail.com>
2021-07-21 15:22:17 -0400

    CMS Updates

--- src/components/equitySection/equitySection.js
+++ src/components/equitySection/equitySection.js
@@ -1,56 +1,40 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { Link, useStaticQuery, graphql } from "gatsby";
+import ReactMarkdown from "react-markdown";
+import { Link } from "gatsby";
 import { getImage } from "gatsby-plugin-image";
 import { BgImage } from "gbimage-bridge";
+import useQuoteImages from "../../hooks/useQuoteImages";
+
+import content from "../../pages/equity.content.yml";
 
 import * as styles from "./equitySection.module.css";
 
 export default function EquitySection({ size = "lg" }) {
-  const { equitySectionImg } = useStaticQuery(
-    graphql`
-      query {
-        equitySectionImg: file(
-          relativePath: { eq: "images/quotes/equity.png" }
-        ) {
-          childImageSharp {
-            gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
-          }
-        }
-      }
-    `
-  );
+  const [{ quoteImagesByPath }] = useQuoteImages();
+
+  const {
+    topQuote: { image },
+    embedded: { description, heading, link },
+  } = content;
 
   return (
     <Row className={styles[size]}>
       <BgImage
         className={styles.background}
-        image={getImage(equitySectionImg.childImageSharp)}
+        image={getImage(quoteImagesByPath[image])}
       >
         <Col
           className={styles.copy}
           md={size === "lg" ? { span: 6, offset: 5 } : null}
         >
-          <h3>OPO Reform is and Urgent Equity Issue</h3>
+          <h3>{heading}</h3>
           {size === "lg" && (
             <>
-              <p>
-                Patients of color are less likely to receive organ transplants
-                than white patients because of inferior OPO service. We must
-                drive reforms to hold OPOs accountable.
-              </p>
-              <p>
-                See our report cataloguing peer-reviewed research on the
-                inequitable service OPOs provide to different ethnic
-                communities. As bipartisan, bicameral Congressional leaders have
-                written, OPO reforms have “urgent implications for health
-                equity”.
-              </p>
+              <ReactMarkdown>{description}</ReactMarkdown>
             </>
           )}
-          <Link to="/equity">
-            We must drive reforms to hold OPOs accountable.
-          </Link>
+          <Link to="/equity">{link}</Link>
         </Col>
       </BgImage>
     </Row>
