9325a5f5a93a6b6ee8fed8b16515266ad89113e7
SammySteiner <sammysteiner@gmail.com>
2021-07-29 19:30:51 -0400

    swapped custom table with react-table

--- src/components/opoTable/opoTable.js
+++ src/components/opoTable/opoTable.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, {useMemo} from "react";
 import { Container, Row, Table } from "react-bootstrap";
-
-import { formatNumber } from "../../utils/utils";
+ import { useTable, useSortBy } from "react-table";
 
 import Tier from "../tier/tier";
 
 import * as styles from "./opoTable.module.css";
 
-export default function OpoTable({
+export default function OpoTableRT({
   citations,
   headings,
   inState = true,
   opos,
   title,
 }) {
-  if (!opos.length) return null;
 
+
+const columns = useMemo(() => {
+  const cols = [
+    "states",
+    "name",
+    "region",
+    "tier",
+    "donors",
+    "shadow",
+    "investigation",
+  ];
   const Heading = ({ color, title }) => (
-    <th scope="col" className={color ?? null}>
+    <>
       {headings[title].heading}
       {citations[title] && (
         <sup>
@@ -30,72 +39,111 @@ export default function OpoTable({
           </a>
         </sup>
       )}
-    </th>
+    </>
   );
+  const createCol = accessor => {
+    let col = {
+      Header: (
+        <Heading
+          title={accessor}
+          color={accessor === "shadow" ? "red" : null}
+        />
+      ),
+      accessor: accessor,
+    };
+    if (accessor === "donors" || accessor === "investigation")
+      col.cellClass = "text-center";
+    if (accessor === "shadow") {
+      col.cellClass = styles.shadows
+      col.color = "red"
+    };
+    if (accessor === "tier") {
+      col.Cell = props => (
+        <Container>
+          <Tier className={styles.tierCol} tier={props.value} />
+        </Container>
+      );
+      col.cellClass = styles.tierCol;
+    }
+    return col;
+  };
+  return cols
+    .map(col => {
+      if (inState && col === "states") return col;
+      if (!inState && (col === "region" || col === "investigation")) return col;
+      return createCol(col);
+    })
+    .filter(head => typeof head !== "string");
+}, [headings, inState, citations]);
 
-  return (
-    <Row className={styles.opoTable}>
-      <h3>{title}</h3>
-      <Table striped>
-        <thead>
-          <tr>
-            {inState ? (
-              <>
-                <Heading title="name" />
-                <Heading title="region" />
-              </>
-            ) : (
-              <>
-                <Heading title="states" />
-                <Heading title="name" />
-              </>
-            )}
-            <Heading title="tier" />
-            <Heading title="donors" />
-            <Heading title="shadow" color="red" />
-            {inState && <Heading title="investigation" />}
-          </tr>
-        </thead>
-        <tbody>
-          {opos.map(
-            ({
-              donors,
-              investigation,
-              name,
-              region,
-              shadows,
-              states,
-              tier,
-            }) => (
-              <tr key={name}>
-                {inState ? (
-                  <>
-                    <td>{name}</td>
-                    <td>{region}</td>
-                  </>
-                ) : (
-                  <>
-                    <td>{states}</td>
-                    <td>{name}</td>
-                  </>
-                )}
-                <td>
-                  <Container>
-                    <Tier className={styles.tierCol} tier={tier} />
-                  </Container>
-                </td>
-                <td className="text-center">{formatNumber(donors)}</td>
-                <td className={styles.shadows}>{formatNumber(shadows)}</td>
-                {inState && (
-                  <td className="text-center">
-                    {investigation ? "Yes" : "--"}
-                  </td>
-                )}
-              </tr>
-            )
-          )}
-        </tbody>
-      </Table>
-    </Row>
-  );
+  const data = useMemo(() => {
+    const formatNumber = (num, options) =>
+      !num || isNaN(num) ? "--" : num.toLocaleString("en-US", options);
+
+    return opos.map(
+      ({ donors, investigation, name, region, shadows, states, tier }) => {
+        return {
+          donors: formatNumber(donors),
+          investigation: investigation ? "Yes" : "--",
+          name: name,
+          region: region,
+          shadow: formatNumber(shadows),
+          states: states,
+          tier: tier,
+        };
+      }
+    );
+  }, [opos]);
+
+   const {
+     getTableProps,
+     getTableBodyProps,
+     headerGroups,
+     rows,
+     prepareRow,
+   } = useTable({ columns, data }, useSortBy);
+
+   const table = (
+     <Row className={styles.opoTable}>
+       <h3>{title}</h3>
+       <Table striped {...getTableProps()}>
+         <thead>
+           {headerGroups.map(headerGroup => (
+             <tr {...headerGroup.getHeaderGroupProps()}>
+               {headerGroup.headers.map(column => (
+                 <th
+                   {...column.getHeaderProps(column.getSortByToggleProps())}
+                   scope="col"
+                   className={column.color ?? null}
+                 >
+                   {column.render("Header")}
+                 </th>
+               ))}
+             </tr>
+           ))}
+         </thead>
+         <tbody {...getTableBodyProps()}>
+           {rows.map(row => {
+             prepareRow(row);
+             return (
+               <tr {...row.getRowProps()}>
+                 {row.cells.map(cell => {
+                   return (
+                     <td
+                       {...cell.getCellProps()}
+                       className={cell.column.cellClass ?? ""}
+                     >
+                       {cell.render("Cell")}
+                     </td>
+                   );
+                 })}
+               </tr>
+             );
+           })}
+         </tbody>
+       </Table>
+     </Row>
+   );
+
+   return table;
 }
