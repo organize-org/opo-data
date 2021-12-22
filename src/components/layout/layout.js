6a698d81753fb2ac6df36c91c1e22e7faa01093d
SMakaiTakori <makait87@icloud.com>
2021-12-22 13:55:15 -0500

    added navigation menu and mobile responsiveness

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { useStaticQuery, graphql, Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import { Breadcrumb, Container, Row } from "react-bootstrap";
 import ReactMarkdown from "react-markdown";
 import Social from "../../components/social/social";
+
+import Navbar from "../navbar/navbar";
 
 import * as styles from "./layout.module.css";
 
@@ -22,32 +24,13 @@ export default function Layout({ crumbLabel, children, sources, social }) {
 
   return (
     <Container fluid>
-      <Row className={styles.header}>
-        <Link to="/" className={styles.logoWithText}>
-          <StaticImage
-            src="../../images/logo.png"
-            alt="logo"
-            placeholder="none"
-          />
-          <div className={styles.logoText}>
-            <h1>{site.siteMetadata.title}</h1>
-            <h2>Performance Comparison</h2>
-          </div>
-        </Link>
-        <Link to="/faqs" className={styles.faqLink}>
-          <p>About Our Organ Donation System</p>
-        </Link>
-      </Row>
-      <Row>
-        {crumbLabel ? (
-          <Breadcrumb className={styles.breadcrumb}>
-            <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-            <Breadcrumb.Item active>{crumbLabel}</Breadcrumb.Item>
-          </Breadcrumb>
-        ) : null}
-        {social ? <Social /> : null}
-      </Row>
-
+      <Navbar site={site.siteMetadata.title} />
+      {crumbLabel ? (
+        <Breadcrumb>
+          <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+          <Breadcrumb.Item active>{crumbLabel}</Breadcrumb.Item>
+        </Breadcrumb>
+      ) : null}
       {children}
       {sources?.length ? (
         <Row className={styles.sources}>
