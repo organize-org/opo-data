104b050d45ce5890efc28da9aa6ed2c434306380
SMakaiTakori <makait87@icloud.com>
2022-01-06 11:50:01 -0500

    added toggle functionality and black background on close

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav } from "react-bootstrap";
 
 import { StaticImage } from "gatsby-plugin-image";
@@ -6,6 +6,33 @@ import { StaticImage } from "gatsby-plugin-image";
 import * as styles from "./navbar.module.css";
 
 export default function Navigation({ site }) {
+  const hamburger = (
+    <StaticImage
+      src="../../images/icons/hamburger.png"
+      alt="hamburger"
+      className={styles.hamburger}
+    />
+  );
+  const close = (
+    <StaticImage
+      src="../../images/icons/close.png"
+      alt="close"
+      className={styles.close}
+    />
+  );
+
+  const [icon, setIcon] = useState(hamburger);
+
+  const toggle = e => {
+    e.preventDefault();
+
+    if (icon.props.alt === "hamburger") {
+      setIcon(close);
+    } else {
+      setIcon(hamburger);
+    }
+  };
+
   return (
     <Navbar expand="lg" variant="dark" className={styles.nav}>
       <Navbar.Brand>
@@ -14,7 +41,6 @@ export default function Navigation({ site }) {
             src="../../images/logo.png"
             alt="logo"
             placeholder="none"
-            className={styles.logo}
           />
           <section className={styles.logoText}>
             <h1>{site}</h1>
@@ -22,13 +48,23 @@ export default function Navigation({ site }) {
           </section>
         </Nav.Link>
       </Navbar.Brand>
-      <section className={styles.mobileTitle}>
+      <section
+        className={
+          icon.props.alt === "hamburger"
+            ? styles.mobileTitle
+            : styles.mobileTitleClose
+        }
+      >
         <h1>Menu</h1>
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
           className={styles.navToggle}
-        />
+          onClick={toggle}
+        >
+          {icon}
+        </Navbar.Toggle>
       </section>
+      }
       <Navbar.Collapse id="basic-navbar-nav" className={styles.navCollapse}>
         <Nav>
           <Nav.Link href="#" className={styles.navLink}>
